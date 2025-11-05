51ef75cd8740268b9ca5d09bb0aba5ddae9c92cf
nagaraju051718 <vakanagaraju718@gmail.com>
2025-11-05 17:08:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,8 @@ def gitDownload(repo)
 {
      git "https://github.com/IntelliqDevops/${repo}.git"
 }
+
+def deployTomcat(jobname,ip,context)
+{
+     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
